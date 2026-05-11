19ad366c2d60eb50b40c0dd38aa47ee3556c325d
Hamjah10 <hamjahmahmud0305@gmail.com>
2026-05-11 21:44:26 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+// PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL HERE:
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwxEY6pcL8ENBJoYFMDPev36RjrSnn_BHun6yJ8UoTBqEI-SaBlXSIAehUnHs_ztl9E/exec";
